7950369474ed80d8de9a8dac32ee7955f56d3046
allwillburn <livefortoday470@gmail.com>
2017-04-04 19:07:09 -0400

    Update riven.lua

--- riven.lua
+++ riven.lua
@@ -152,7 +152,7 @@ OnTick(function (myHero)
             end		
 			
 	    if RivenMenu.Combo.Emote:Value() then
-			CastEmote(1)
+			CastEmote(2)
             end		
 			    
             if RivenMenu.Combo.AA:Value() and ValidTarget(target, 125) then
